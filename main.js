d7d64f0ef49c8a5d48b372477c5a4aacb76b3b48
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-03-28 23:55:49 -0300

    Adicionado arquivo com cores e nova funcionalidade no javascript

--- main.js
+++ main.js
@@ -1,33 +1,83 @@
-const LinksSocialMedia = {
+const socialMediaLinks = {
   github: 'felipefama',
-  youtube: '5PXcfljqAuwc_MWfw1eNlQ',
+  linkedin: 'felipe-fama',
   facebook: 'lipeh.fama',
   instagram: 'lipeh.mesquita',
   twiter: 'lipehfama'
 }
 
-function changeSocialMedialLinks() {
+const changeSocialMedialLinks = () => {
   for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
+    const socialMedia = li.getAttribute('class');
 
-    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    if (socialMedia === 'linkedin')
+      li.children[0].href = `https://${socialMedia}.com/in/${socialMediaLinks[socialMedia]}`;
+
+    else
+      li.children[0].href = `https://${socialMedia}.com/${socialMediaLinks[socialMedia]}`;
   }
 }
 
-changeSocialMedialLinks()
+changeSocialMedialLinks();
 
-function getGithubProfileInfos() {
-  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+const getGitHubProfileData = (input = socialMediaLinks.github) => {
+  const favIcon = document.querySelector("link[rel='shortcut icon']");
+
+  const url = `https://api.github.com/users/${input}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      userName.textContent = data.name
-      userBio.textContent = data.bio
-      userLink.href = data.html_url
-      userImage.src = data.avatar_url
-      userLogin.textContent = data.login
-    })
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+
+      favIcon.href = data.avatar_url;
+      document.title = `${data.name} | NLW Heat 2021`;
+    });
 }
 
-getGithubProfileInfos()
+const searchButton = document.querySelector('#search-button');
+const githubNickInput = document.querySelector('#github-nick-input');
+
+const getGithubUserInput = () => {
+  const githubUserNick = githubNickInput.value;
+
+  if (!githubUserNick) return;
+
+  else getGitHubProfileData(githubUserNick);
+}
+
+searchButton.addEventListener('click', getGithubUserInput);
+
+githubNickInput.addEventListener('keyup', (event) => {
+
+  let key = event.keyCode;
+
+  if (key === 13) getGithubUserInput();
+
+});
+
+getGitHubProfileData();
+
+const optionSearch = document.querySelector('#option-search');
+optionSearch.addEventListener('click', () => displayBox('option-box'));
+
+const displayBox = (boxId) => {
+  const box = document.querySelector(`#${boxId}`);
+
+  box.classList.add('display');
+
+  box.addEventListener('click', e => {
+    const el = e.target;
+    if (el.id == boxId) {
+      box.classList.remove('display');
+    }
+  });
+
+  const closeButton = document.querySelector('#close-box');
+  closeButton.addEventListener('click', () => box.classList.remove('display'));
+
+}
